3d95cf8212830ffd1388851659f88d99b04bd7d7
Clément NGUYEN <clement.nguyen@epita.fr>
2022-10-29 01:06:42 +0200

    index.js: add default index

---
+++ index.js
@@ -0,0 +1,15 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only one)
+// We use 'c' for the event parameter to keep it seperate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with the client's token
+client.login(token);
