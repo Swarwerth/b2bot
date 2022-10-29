94f24906c5d282dc4079b6e8d149d0f17bc96429
Clément NGUYEN <clement.nguyen@epita.fr>
2022-10-29 16:18:11 +0200

    follow the discord.js guide

---
+++ deploy-commands.js
@@ -0,0 +1,38 @@
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+const fs = require('node:fs');
+
+const commands = [];
+
+// Grab all the command files from the commands directory
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Grab the SlashCommandBuilder output of each command's data for deployment
+for (const file of commandFiles)
+{
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try
+    {
+        console.log(`Started refreshing ${commands.length} applications (/) commands.`);
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            // Routes.applicationCommads(clientId),
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} applications (/) commands.`);
+    }
+
+    catch(error)
+    {
+        console.error(error);
+    }
+})();
